d0185efc619c3b31bc8505f56d177b0fa080546e
Dum <91747868+Dum121@users.noreply.github.com>
2024-01-24 22:18:02 +0700

    Update Auto Drop Fruit.lua

--- Grand Piece Online /Auto Drop Fruit.lua
+++ Grand Piece Online /Auto Drop Fruit.lua
@@ -6,7 +6,14 @@ local dropleg = {"Goro",
 -- // myth fruit list
 local dropmyth = {"Ope","Mochi","Venom","Tori","Buddha"}
 
--- function
+-- // Virtual
+local vim = game:GetService('VirtualInputManager')
+function drop()
+        vim:SendKeyEvent(true, Enum.KeyCode.Backspace, false, nil)
+	task.wait(0.1)
+        vim:SendKeyEvent(false, Enum.KeyCode.Backspace, false, nil)
+    end
+-- // function
 if not getgenv().execute then getgenv().execute = true else return end
 spawn(function()
 	while wait() do
@@ -18,12 +25,9 @@ if getgenv().autodropleg then
 	wait(getgenv().droplegdelay)
       game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack[v.Name])
 	wait(0.6)
-	        local args = {
-            [1] = "drop",
-            [2] = game.Players.LocalPlayer.Character:FindFirstChild(v.Name)
-        }
-
-        game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Tools"):InvokeServer(unpack(args))
+	if not game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") and table.find(dropleg,v.Name) then
+	drop()
+	end
 wait(0.7)
 		end
 	end
@@ -35,12 +39,9 @@ if getgenv().autodropmyth then
 	wait(getgenv().dropmythdelay)
       game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack[v.Name])
 	wait(0.6)
-	        local args = {
-            [1] = "drop",
-            [2] = game.Players.LocalPlayer.Character:FindFirstChild(v.Name)
-        }
-
-        game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Tools"):InvokeServer(unpack(args))
+	if not game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") and table.find(dropmyth,v.Name) then
+	drop()
+	end
 wait(0.7)
 		end
 	end
